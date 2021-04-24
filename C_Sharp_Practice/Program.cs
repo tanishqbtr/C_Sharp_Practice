5cfc6813f77184baee9b5ff7264d2c9a88a0dd2f
tanishqbtr <tanishqbtr@gmail.com>
2021-04-24 23:46:07 +0530

    D

--- C_Sharp_Practice/Program.cs
+++ C_Sharp_Practice/Program.cs
@@ -23,10 +23,14 @@ namespace C_Sharp_Practice
             Console.WriteLine(uname.Contains("ish"));
             Console.WriteLine(uname.IndexOf("i")); //Only lists the first one, -1 if not present
             Console.WriteLine(uname.Substring(2,3)); //Initial Inedex, length of next characters
+            Console.WriteLine(Math.Sqrt(36));
 
-
-
-
+            Console.Write("Please Enter you name : ");
+            string uname2 = Console.ReadLine();
+            Console.WriteLine(uname2);
+            string age2 = "24";
+            int age3 = Convert.ToInt32(age2);
+            Console.WriteLine("Your age + 10 = "+(age3+10));
 
 
             Console.ReadLine();
