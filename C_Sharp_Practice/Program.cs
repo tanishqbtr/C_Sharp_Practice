a3d431753cb3e8e7af95553dd6744558e0200096
tanishqbtr <tanishqbtr@gmail.com>
2021-04-24 23:30:55 +0530

    essentials

--- C_Sharp_Practice/Program.cs
+++ C_Sharp_Practice/Program.cs
@@ -6,7 +6,18 @@ namespace C_Sharp_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string uname = "Tanishq";
+            int age = 23;
+            char grade = 'A';
+            double d1= 3.0;
+            bool isMale = true;
+
+
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("Hello!" + uname + "Your age is" + age);
+
+            Console.WriteLine(uname[3]);
+            Console.ReadLine();
         }
     }
 }
